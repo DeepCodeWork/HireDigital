eef10750d8b51d822165eb5dc08e468c04ec29f8
makingout456 <kumardeep245@gmail.com>
2021-05-05 21:45:10 +0530

    completed framework for home, navbar and footer component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
+import React from 'react';
+import './bootstrap.min.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import {Container} from 'react-bootstrap';
+import Home from './views/Home';
+import { BrowserRouter, Route } from 'react-router-dom';
+import ProductDetail from './views/ProductCard';
+
 function App() {
   return (
-    <div className="App">
-      <div>HireDigital</div>
-    </div>
+    <BrowserRouter>
+      <Header/>
+        <main className='py-3'>
+          <Container>
+            <Route path='/' component={Home} exact/>
+            <Route path='/product/:id' component={ProductDetail}/>
+          </Container>
+        </main>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/views/Home.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react'
+import { Col, Row } from 'react-bootstrap';
+import Products from '../temp/products.json';
+import axios from 'axios';
+
+const Home = () => { 
+    return (
+        <>
+            <Row>
+                Products
+            </Row> 
+        </>
+    )
+}
+
+export default Home
